3629324e193e547698eaf599a8160ce17440ea6c
Alexsiiassa <95941253+Alexsiiassa@users.noreply.github.com>
2022-07-05 00:30:28 +0300

    1234

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
 
 
-@require_http_methods(["GET"])
 def index(request):
-    return HttpResponse(request, 'У меня получилось!')
+    return HttpResponse(request, 'infra_project/index.html')
 
 
-@require_http_methods(["GET"])
 def second_page(request):
-    return HttpResponse(request, 'А это вторая страница')
+    return HttpResponse(request, 'infra_project/second_page.html')
