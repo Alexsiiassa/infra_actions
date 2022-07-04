96e8f0c9dbb3dad08125461b1b5c22dfdf799bec
Alexsiiassa <95941253+Alexsiiassa@users.noreply.github.com>
2022-07-05 00:18:52 +0300

    1234

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,12 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    real_count = 1
-    text_repe = 'У меня получилось!'
-    return HttpResponse(request, real_count, text_repe)
+    return HttpResponse(request, 'У меня получилось!')
 
 
 def second_page(request):
-    real_count = 1
-    text_repe = 'А это вторая страница'
-    return HttpResponse(request, real_count, text_repe)
+    return HttpResponse(request, 'А это вторая страница')
