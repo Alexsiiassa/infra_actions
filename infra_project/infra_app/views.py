02e77321a77ff7fc7c856c43d4686595a6767e22
Alexsiiassa <95941253+Alexsiiassa@users.noreply.github.com>
2022-07-05 00:47:31 +0300

    1234

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(request, 'У меня получилось!',)
+    return HttpResponse(request, 'infra_project/index.html')
 
 
 def second_page(request):
-    return HttpResponse(request, 'А это вторая страница',)
+    return HttpResponse(request, 'А это вторая страница')
