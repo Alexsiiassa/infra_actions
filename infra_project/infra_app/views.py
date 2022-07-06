5cc2b5391dda90febbf040d45d153bff02955def
Alexiiassa <mackserm09@yandex.ru>
2022-07-07 00:56:11 +0300

    001

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(request, 'У меня получилось!')
+    return HttpResponse(request, 'У меня получилось!', status_code=200)
 
 
 def second_page(request):
-    return HttpResponse(request, 'А это вторая страница')
+    return HttpResponse(request, 'А это вторая страница', status_code=200)
